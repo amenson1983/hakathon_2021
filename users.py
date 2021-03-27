0409bf4d60791aba7638741254582d69c513798c
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2021-03-27 11:21:18 +0200

    27/03/2021 - 11:21 - classification users

--- users.py
+++ users.py
@@ -9,7 +9,21 @@ class CUser:
         self.phone = phone
     def __str__(self):
         return f"Фамилия: {self.surname}\nИмя: {self.name}\nОтчество: {self.father_name}\nАдрес: {self.adress}\nТел.:{self.phone}"
-#class CDoctor(CUser):
+class CDoctor():
+    def __init__(self,surname,name,father_name,adress,phone,doc_speciality,hospital_name,hospital_adress,login,password):
+        self.password = password
+        self.login = login
+        self.hospital_adress = hospital_adress
+        self.hospital_name = hospital_name
+        self.doc_speciality = doc_speciality
+        self.surname = surname
+        self.name = name
+        self.father_name = father_name
+        self.adress = adress
+        self.phone = phone
+
+    def __str__(self):
+        return f"Фамилия: {self.surname}\nИмя: {self.name}\nОтчество: {self.father_name}\nАдрес: {self.adress}\nТел.:{self.phone}\nСпециальность:{self.doc_speciality}\nГоспиталь:{self.hospital_name}\nГоспиталь:{self.hospital_adress}\nЛогин:{self.login}\nПароль:{self.password}"
 
 
 
@@ -19,5 +33,12 @@ if __name__ == '__main__':
     father_name = "Викторович"
     adress = "Житомир, ул.Замковая, 1"
     phone  = "0989922947"
+    login = "login"
+    hospital_adress = "hospital_adress"
+    hospital_name = "hospital_name"
+    doc_speciality = "doc_speciality"
+    password = "123"
     x = CUser(surname,name,father_name,adress,phone)
     print(x.__str__())
+    z = CDoctor(surname,name,father_name,adress,phone,doc_speciality,hospital_name,hospital_adress,login,password)
+    print(z)
