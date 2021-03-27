a0fc50a22977e4e12af7076b27739898ad498bc3
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2021-03-27 12:00:14 +0200

    27/03/2021 - 11:59 - classification users

--- users.py
+++ users.py
@@ -1,7 +1,8 @@
 
 
 class CUser:
-    def __init__(self,surname,name,father_name,adress,phone):
+    def __init__(self, surname,name,father_name,adress,phone):
+
         self.surname = surname
         self.name = name
         self.father_name = father_name
@@ -10,7 +11,7 @@ class CUser:
     def __str__(self):
         return f"Фамилия: {self.surname}\nИмя: {self.name}\nОтчество: {self.father_name}\nАдрес: {self.adress}\nТел.:{self.phone}"
 class CDoctor():
-    def __init__(self,surname,name,father_name,adress,phone,doc_speciality,hospital_name,hospital_adress,login,password):
+    def __init__(self, surname,name,father_name,adress,phone,doc_speciality,hospital_name,hospital_adress,login,password):
         self.password = password
         self.login = login
         self.hospital_adress = hospital_adress
@@ -38,9 +39,33 @@ class CProvisor:
     def __str__(self):
         return f"Фамилия: {self.surname}\nИмя: {self.name}\nОтчество: {self.father_name}\nАдрес: {self.adress}\nТел.:{self.phone}\nID провизора:{self.id_card}\nАптека:{self.pharmacy_name}\nАптека адрес: {self.pharmacy_adress}"
 
+class CCustomer:
+    def __init__(self, surname,name,father_name,phone,id_number_or_passport):
+        self.id_number_or_passport = id_number_or_passport
+        self.surname = surname
+        self.name = name
+        self.father_name = father_name
+        self.phone = phone
+    def __str__(self):
+        return f"Фамилия: {self.surname}\nИмя: {self.name}\nОтчество: {self.father_name}\nТел.:{self.phone}\nID: {self.id_number_or_passport}"
+
 #class CDoctor_workout:
     #def registration(self):
     #def login_to_system(self):
+
+class Clogging:
+    def __init__(self,login,password):
+        self.login = login
+        self.password = password
+class Clogging_workout:
+    def logging_to_system(self,login,password,logins_dict):
+        if login in logins_dict:
+            if password == logins_dict[password]:
+                print('OK')
+            else: print('Password is incorrect')
+        else: print('No such user')
+
+
 
 
 if __name__ == '__main__':
@@ -61,5 +86,7 @@ if __name__ == '__main__':
     #print(user.__str__())
     #doc = CDoctor(surname,name,father_name,adress,phone,doc_speciality,hospital_name,hospital_adress,login,password)
     #print(doc)
-    provisor = CProvisor(surname,name,father_name,adress,phone,id_card,pharmacy_name,pharmacy_adress)
-    print(provisor)
+    #provisor = CProvisor(surname,name,father_name,adress,phone,id_card,pharmacy_name,pharmacy_adress)
+    #print(provisor)
+    customer = CCustomer(surname,name,father_name,phone,id_card)
+    print(customer)
